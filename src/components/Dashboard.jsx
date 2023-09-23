d0c03027a574dc503be8f172bd280a1d254218e0
Fgonzalez409 <fergonz409@gmail.com>
2023-09-23 15:33:10 -0500

    attempting to fetch data

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,10 +5,12 @@ import { useEffect } from 'react'
 
 const Dashboard = () => {
 
+  const API_URL = "https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key="
   const API_KEY = "yAIutPvgJDxgqt83ZQLp8WCKfrmMtQ5BDQE7x9iG"
+
     const [data,setData] = useState([])
     useEffect(() => {
-        fetch(`https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=${API_KEY}`)
+        fetch(`"https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=" + ${API_KEY}`)
         .then((res) => res.json())
         .then((data) => setData(data))
     }, [])
