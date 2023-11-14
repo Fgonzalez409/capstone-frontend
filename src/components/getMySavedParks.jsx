d27c0d15e5431f797224cb6d0911629003e5a3ad
Fgonzalez409 <fergonz409@gmail.com>
2023-11-13 18:11:41 -0600

    updated getMySavedParks route

--- src/components/getMySavedParks.jsx
+++ src/components/getMySavedParks.jsx
@@ -36,7 +36,7 @@ const GetMySavedParks = (props) => {
         <div>
           {savedPark.map((park, index) => (
             <div key={index} id="park-info-container">
-              <h2>{park.id}</h2>
+              <h2>{park.park_id}</h2>
               <p>Park Code: {park.parkCode}</p>
               <div>
                 {park.image}
